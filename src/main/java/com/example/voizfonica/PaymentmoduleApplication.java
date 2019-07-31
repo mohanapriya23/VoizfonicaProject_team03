c585140f301491787ef2c19b6e6f6b4381cb81ed
KrishnaPranavA <ayyagarikrishnapranav@gmail.com>
2019-07-31 16:41:53 +0530

    Register commited

---
+++ src/main/java/com/example/voizfonica/PaymentmoduleApplication.java
@@ -0,0 +1,13 @@
+package com.example.voizfonica;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PaymentmoduleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PaymentmoduleApplication.class, args);
+    }
+
+}
